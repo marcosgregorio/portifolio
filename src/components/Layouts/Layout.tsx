9103ced3d9c4bdad8e83df8514de5ddd4df86bc6
Marcos Gregorio <marquinhogreg2013@gmail.com>
2024-06-24 15:04:14 -0300

    feat criando o footer

--- src/components/Layouts/Layout.tsx
+++ src/components/Layouts/Layout.tsx
@@ -1,14 +1,24 @@
 import React from "react";
-import { Container, FooterBar, List, ListFooter, ListOption, MenuLink, NavBar } from "./LayoutStyles";
-import gmailLogo from "../../assets/gmail-logo.png"
-import githubLogo from "../../assets/github-sign.png"
-import linkedinLogo from "../../assets/social.png"
+import {
+  Container,
+  FooterBar,
+  List,
+  ListFooter,
+  ListOption,
+  ListOptionFooter,
+  MenuLink,
+  NavBar,
+  SocialMediaLogo,
+} from "./LayoutStyles";
+import gmailLogo from "../../assets/gmail-logo.png";
+import githubLogo from "../../assets/github-sign.png";
+import linkedinLogo from "../../assets/social.png";
 
 type LayoutProps = {
-  children: React.ReactNode
+  children: React.ReactNode;
 };
 
-export const Layout: React.FC<LayoutProps> = ({children}) => {
+export const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
     <Container>
       <NavBar>
@@ -27,9 +37,21 @@ export const Layout: React.FC<LayoutProps> = ({children}) => {
       {children}
       <FooterBar>
         <ListFooter>
-          <ListOption><a href=""> <img src={githubLogo}/> </a></ListOption>
-          <ListOption><a href=""> <img src={gmailLogo}/> </a></ListOption>
-          <ListOption><a href=""> <img src={linkedinLogo}/> </a></ListOption>
+          <ListOptionFooter>
+            <a href="https://github.com/marcosgregorio" target="_blank">
+              <SocialMediaLogo src={githubLogo} />
+            </a>
+          </ListOptionFooter>
+          <ListOptionFooter>
+            <a href="">
+              <SocialMediaLogo src={gmailLogo} />{" "}
+            </a>
+          </ListOptionFooter>
+          <ListOptionFooter>
+            <a href="">
+              <SocialMediaLogo src={linkedinLogo} />{" "}
+            </a>
+          </ListOptionFooter>
         </ListFooter>
       </FooterBar>
     </Container>
--- src/components/Layouts/LayoutStyles.ts
+++ src/components/Layouts/LayoutStyles.ts
@@ -2,12 +2,12 @@ import { Link } from "react-router-dom";
 import styled from "styled-components";
 
 export const Container = styled.div`
-    /* display: grid;
+  /* display: grid;
     grid-template-areas:
     "AS AS"
     "AS CT"; */
-    height: 100vh;
-    background-color: #011;
+  height: 100vh;
+  background-color: #011;
 `;
 
 export const NavBar = styled.nav`
@@ -39,11 +39,26 @@ export const ListOption = styled.li`
     color: ${(props) => props.theme.colors.white};
     text-decoration: none;
     padding: 1em;
-    > img {
-      width: 2em;
-      height: 2em;
-    }
   }
+`;
+
+export const ListOptionFooter = styled.li`
+  > a {
+    transition: all 0.3s;
+    color: ${(props) => props.theme.colors.white};
+    text-decoration: none;
+    padding: 1em;
+  }
+`;
+
+export const SocialMediaLogo = styled.img`
+  transition: all 0.3s;
+  &:hover {
+    transform: translateY(-5px);
+  }
+  width: 2em;
+  height: 2em;
+  align-items: center;
 `;
 
 export const FooterBar = styled.nav`
@@ -67,6 +82,4 @@ export const ListFooter = styled.ul`
   border-radius: 0px 25px 0px 0px;
 `;
 
-export const MenuLink = styled(Link)`
-
-`
+export const MenuLink = styled(Link)``;
